6f128b006a6d26ce949f08c21bab04bab554b65c
Michał Jarzyna <jarzyna.michal@hotmail.com>
2016-02-07 19:04:19 +0100

    add missing modifications from earlier reafactoring commit

--- src/Rescuer/Rescuer.Management/WindowsService/Shell/IWindowsServiceShell.cs
+++ src/Rescuer/Rescuer.Management/WindowsService/Shell/IWindowsServiceShell.cs
@@ -1,7 +1,8 @@
-namespace Rescuer.Management
+namespace Rescuer.Management.WindowsService
 {
     public interface IWindowsServiceShell
     {
         string GetServiceStatus(string serviceName);
+        bool CheckExisting(string isAny);
     }
 }
