4c62a62c8a2c03296762ee6b2f139a908543b8fd
Michał Jarzyna <jarzyna.michal@hotmail.com>
2016-02-07 19:02:39 +0100

    add missed rescuer registration to DI

--- src/Rescuer/Rescuer.Management.Tests/WindowsServiceRescuerTests.cs
+++ src/Rescuer/Rescuer.Management.Tests/WindowsServiceRescuerTests.cs
@@ -1,66 +1,55 @@
-﻿using System;
-using Autofac;
-using Autofac.Features.ResolveAnything;
+﻿using Autofac;
 using Moq;
 using NUnit.Framework;
-using Assert = NUnit.Framework.Assert;
-
+using Rescuer.Management.WindowsService;
 
 namespace Rescuer.Management.Tests
 {
     [TestFixture]
-    public class ManagementWindowsServiceTests
+    public class WindowsServiceRescuerTests
     {
-        private ContainerBuilder _builder;
-
-        [SetUp]
-        public void Config()
-        {
-            _builder = new ContainerBuilder();
-            _builder.RegisterType<RescuerFactory>().As<IRescuerFactory>();
-
-        }
-
-
         [Test]
-        public void Can_Inject_MockedShell_To_WindwosServiceRescuer()
+        public void Can_Check_StoppedStatus_WindowsService_Test()
         {
+            var builder = new ContainerBuilder();
             var windowsServiceShell = new Mock<IWindowsServiceShell>();
-            int shellHits = 0;
-            windowsServiceShell.Setup(p => p.GetServiceStatus(String.Empty))
-                .Callback(() => { shellHits++; })
-                .Returns("working");
+            
+            windowsServiceShell.Setup(p => p.GetServiceStatus(It.IsAny<string>()))
+                .Returns("stopped");
 
-            using (var container = _builder.Build())
+            builder.RegisterInstance(windowsServiceShell.Object).As<IWindowsServiceShell>();
+            builder.RegisterType<WindowsServiceRescuer>().AsImplementedInterfaces();
+
+            using (var container = builder.Build())
             {
-                var factory = container.Resolve<IRescuerFactory>();
-                var rescuer = factory.Create("rescuerTestService", RescuerType.WindowsService);
+                IRescuer rescuer = container.Resolve<IWindowsServiceRescuer>();
 
-                rescuer.CheckHealth();
+                rescuer.ConnectToService("RescuerTestService");
+                var healthStatus = rescuer.CheckHealth();
 
-                Assert.IsTrue(shellHits > 0, "Windows service rescuer should hit mocked shell at least once.");
+                Assert.IsNotNull(healthStatus);
+                Assert.AreEqual(HealthStatus.Stopped, healthStatus);
             }
-        }                      
+        }
 
         [Test]
         public void Can_Check_WorkingStatus_WindowsService_Test()
         {
-            string serviceName = "RescuerTestService";
-
+            var builder = new ContainerBuilder();
             var windowsServiceShell = new Mock<IWindowsServiceShell>();
 
             windowsServiceShell.Setup(p => p.GetServiceStatus(It.IsAny<string>()))
-                .Returns("working");                
+                .Returns("working");
 
-            _builder.RegisterInstance(windowsServiceShell).As<IWindowsServiceShell>();
+            builder.RegisterInstance(windowsServiceShell.Object).As<IWindowsServiceShell>();
+            builder.RegisterType<WindowsServiceRescuer>().AsImplementedInterfaces();
 
-            using (var container = _builder.Build())
-            {                
-                var factory = container.Resolve<IRescuerFactory>();
+            using (var container = builder.Build())
+            {
+                IRescuer rescuer = container.Resolve<IWindowsServiceRescuer>();
 
-                IRescuer wsRescuer = factory.Create(serviceName, RescuerType.WindowsService);
-
-                HealthStatus healthStatus = wsRescuer.CheckHealth();
+                rescuer.ConnectToService("RescuerTestService");
+                var healthStatus = rescuer.CheckHealth();
 
                 Assert.IsNotNull(healthStatus);
                 Assert.AreEqual(HealthStatus.Working, healthStatus);
@@ -68,30 +57,69 @@ namespace Rescuer.Management.Tests
         }
 
         [Test]
-        public void Can_Check_StoppedStatus_WindowsService_Test()
+        public void Can_Connect_ToService_Test()
         {
-            string serviceName = "RescuerTestService";
-
+            var builder = new ContainerBuilder();
             var windowsServiceShell = new Mock<IWindowsServiceShell>();
 
-            windowsServiceShell.Setup(p => p.GetServiceStatus(It.IsAny<string>()))
-                .Returns("stopped");
+            windowsServiceShell.Setup(p => p.CheckExisting(It.IsAny<string>()))
+                .Returns(true);
 
-            _builder.RegisterInstance(windowsServiceShell).As<IWindowsServiceShell>();
+            builder.RegisterInstance(windowsServiceShell.Object).As<IWindowsServiceShell>();
+            builder.RegisterType<WindowsServiceRescuer>().AsImplementedInterfaces();
 
-            using (var container = _builder.Build())
+            using (var container = builder.Build())
             {
-                var factory = container.Resolve<IRescuerFactory>();
+                var rescuer = container.Resolve<IWindowsServiceRescuer>();
 
-                IRescuer wsRescuer = factory.Create(serviceName, RescuerType.WindowsService);
-
-                HealthStatus healthStatus = wsRescuer.CheckHealth();
-
-                Assert.IsNotNull(healthStatus);
-                Assert.AreEqual(HealthStatus.Stopped, healthStatus);
+                Assert.DoesNotThrow(() => rescuer.ConnectToService("test"));
             }
         }
 
-        
+        [Test]
+        public void Can_Handle_Connection_To_NoExistingService_Test()
+        {
+            var builder = new ContainerBuilder();
+            var windowsServiceShell = new Mock<IWindowsServiceShell>();
+
+            windowsServiceShell.Setup(p => p.CheckExisting(It.IsAny<string>()))
+                .Returns(false);
+
+            builder.RegisterInstance(windowsServiceShell.Object).As<IWindowsServiceShell>();
+            builder.RegisterType<WindowsServiceRescuer>().AsImplementedInterfaces();
+
+            using (var container = builder.Build())
+            {
+                var rescuer = container.Resolve<IWindowsServiceRescuer>();
+
+                Assert.Catch<ServiceNotFoundException>(() => rescuer.ConnectToService("test"));
+            }
+        }
+
+        [Test]
+        public void Can_Inject_MockedShell_To_WindwosServiceRescuer_Test()
+        {
+            var builder = new ContainerBuilder();
+
+            var windowsServiceShell = new Mock<IWindowsServiceShell>();
+
+            var shellHits = 0;
+            windowsServiceShell.Setup(p => p.GetServiceStatus(string.Empty))
+                .Callback(() => { shellHits++; })
+                .Returns("working");
+
+            builder.RegisterInstance(windowsServiceShell.Object).As<IWindowsServiceShell>();
+            builder.RegisterType<WindowsServiceRescuer>().AsImplementedInterfaces();
+
+            using (var container = builder.Build())
+            {
+                var rescuer = container.Resolve<IWindowsServiceRescuer>();
+
+                rescuer.ConnectToService("RescuerTestService");
+                rescuer.CheckHealth();
+
+                Assert.IsTrue(shellHits > 0, "Windows service rescuer should hit mocked shell at least once.");
+            }
+        }
     }
 }
