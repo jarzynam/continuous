739be3f88a4ae4ac92112b2fb9bad863e95cdfdc
Michał Jarzyna <jarzyna.michal@hotmail.com>
2016-02-13 11:54:23 +0100

    added Rescuer.Service and RescuerManagementModule (with tests)

---
+++ src/Rescuer/Rescuer.Management.Tests/RescuerManagementModuleTests.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using NUnit.Framework;
+using Rescuer.Management.WindowsService;
+
+namespace Rescuer.Management.Tests
+{
+    [TestFixture]
+    class RescuerManagementModuleTests
+    {
+        [Test]
+        public void Can_Build_RescuerManagementModule_Test()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule<RescuerManagementModule>();
+
+            using (var container = builder.Build())
+            {
+                IWindowsServiceRescuer rescuer = null;
+
+                Assert.DoesNotThrow(() =>
+                {
+                    rescuer = container.Resolve<IWindowsServiceRescuer>();
+                }, "Cant load all components to windows service rescuer" );
+                    
+
+                Assert.IsNotNull(rescuer, "rescuer instance cant be null");
+            }
+        }
+    }
+}
---
+++ src/Rescuer/Rescuer.Management/RescuerManagementModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Rescuer.Management.WindowsService;
+using Rescuer.Management.WindowsService.Shell;
+
+namespace Rescuer.Management
+{
+    public class RescuerManagementModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<WindowsServiceRescuer>().As<IWindowsServiceRescuer>();
+            builder.RegisterType<WindowsServiceShell>().As<IWindowsServiceShell>();
+
+        }
+    }
+}
