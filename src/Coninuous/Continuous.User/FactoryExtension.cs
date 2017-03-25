1d36b09735db403de863ef027ab8996f59ee369d
Michał Jarzyna <jarzyna.michal@hotmail.com>
2017-03-25 19:22:33 +0100

    added factory extensions for user and user group

---
+++ src/Coninuous/Continuous.User/FactoryExtension.cs
@@ -0,0 +1,19 @@
+﻿using Continuous.Management;
+using Continuous.User.LocalUserGroups;
+using Continuous.User.Users;
+
+namespace Continuous.User
+{
+    public static class FactoryExtension
+    {
+        public static IUserShell UserShell(this ContinuousManagementFactory factory)
+        {
+            return new UserShell();
+        }
+
+        public static ILocalUserGroupShell LocalUserGroup (this ContinuousManagementFactory factory)
+        {
+            return new LocalUserGroupShell();
+        }
+    }
+}
