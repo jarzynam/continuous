57196c7843bdea445ff6ac151c8983a7e2239042
Michał Jarzyna <jarzyna.michal@hotmail.com>
2017-04-03 18:57:56 +0200

    added missing file

---
+++ src/Coninuous/Continuous.User/Users/Model/UserModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Continuous.User.Users.Model
+{
+    /// <summary>
+    /// User representation 
+    /// </summary>
+    public class UserModel
+    {
+        /// <summary>
+        /// User name
+        /// </summary>
+        public string Name { get; set; }
+        
+        /// <summary>
+        /// User full name
+        /// </summary>
+        public string FullName { get; set; }
+        
+        /// <summary>
+        /// User description
+        /// </summary>
+        public string Description { get; set; }
+        
+        /// <summary>
+        /// User Password - will be empty for getUser
+        /// </summary>
+        public string Password { get; set; }
+        
+        /// <summary>
+        /// User accout expiration date - if null, the account will never exipre
+        /// </summary>
+        public DateTime? Expires { get; set; }
+    }
+}
