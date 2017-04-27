f4a8346c2bf474931c06e15041e4c59caf6ebd7d
Michał Jarzyna <jarzyna.michal@hotmail.com>
2017-04-27 23:17:00 +0200

    added delay-on-demand option in basic service implementation and coping executables to test project as postbuild events

--- src/Continuous.WindowsService/Continuous.Test.BasicService/ContinuousBasicService.cs
+++ src/Continuous.WindowsService/Continuous.Test.BasicService/ContinuousBasicService.cs
@@ -1,10 +1,14 @@
-﻿using System.ServiceProcess;
+﻿using System.Configuration;
+using System.ServiceProcess;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Continuous.Test.BasicService
 {
     public partial class ContinuousBasicService : ServiceBase
     {
         private readonly FileWriter _writer;
+        private readonly int _delayTime;
 
         public ContinuousBasicService()
         {
@@ -13,25 +17,35 @@ namespace Continuous.Test.BasicService
             _writer = new FileWriter();
 
             CanPauseAndContinue = true;
+
+            _delayTime = int.Parse(ConfigurationManager.AppSettings["DelayTime"]);
         }
 
         protected override void OnStart(string[] args)
         {
+            RequestAdditionalTime(_delayTime);
+            Thread.Sleep(_delayTime);
             _writer.LogStart();
         }
         
         protected override void OnStop()
         {
+            RequestAdditionalTime(_delayTime);
+            Thread.Sleep(_delayTime);
             _writer.LogEnd();
         }
 
         protected override void OnPause()
         {
+            RequestAdditionalTime(_delayTime);
+            Thread.Sleep(_delayTime);
             _writer.LogPause();
         }
 
         protected override void OnContinue()
         {
+            RequestAdditionalTime(_delayTime);
+            Thread.Sleep(_delayTime);
             _writer.LogContinue();
         }
     }
