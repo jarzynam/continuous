0b1fa339dea1864ba708033c8bf1c7d350a373b6
Michał Jarzyna <jarzyna.michal@hotmail.com>
2017-05-29 22:59:03 +0200

    disabled possibility to directly set windows service info properties from outside of the project

--- src/Continuous.WindowsService/Continuous.WindowsService/Model/WindowsServiceInfo.cs
+++ src/Continuous.WindowsService/Continuous.WindowsService/Model/WindowsServiceInfo.cs
@@ -32,92 +32,92 @@ namespace Continuous.WindowsService.Model
         /// <summary>
         /// Service name
         /// </summary>
-        public string Name { get; set; }
+        public string Name { get; internal set; }
 
         /// <summary>
         /// Service display name
         /// </summary>
-        public string DisplayName { get; set; }
+        public string DisplayName { get; internal set; }
 
         /// <summary>
         /// Account name - LocalSystem as default
         /// </summary>
-        public string AccountName { get; set; }
+        public string AccountName { get; internal set; }
 
         /// <summary>
         /// Account domain - '.' as local domain
         /// </summary>
-        public string AccountDomain { get; set; }
+        public string AccountDomain { get; internal set; }
 
         /// <summary>
         /// Service description
         /// </summary>
-        public string Description { get; set; }
+        public string Description { get; internal set; }
 
         /// <summary>
         /// Id of process using by this service
         /// </summary>
-        public int ProcessId { get; set; }
+        public int ProcessId { get; internal set; }
 
         /// <summary>
         /// Windows service status
         /// </summary>
-        public WindowsServiceStatus Status { get; set; }
+        public WindowsServiceStatus Status { get; internal set; }
 
         /// <summary>
         /// Service start mode
         /// </summary>
-        public WindowsServiceStartMode StartMode { get; set; }
+        public WindowsServiceStartMode StartMode { get; internal set; }
 
         /// <summary>
         /// Type of the process responsible for running this service
         /// </summary>
-        public WindowsServiceType Type { get; set; }
+        public WindowsServiceType Type { get; internal set; }
 
         /// <summary>
         /// Current state of this service
         /// </summary>
-        public WindowsServiceState State { get; set; }
+        public WindowsServiceState State { get; internal set; }
 
         /// <summary>
         /// Serverity of the error throwed in service installation process
         /// </summary>
-        public WindowsServiceErrorControl ErrorControl { get; set; }
+        public WindowsServiceErrorControl ErrorControl { get; internal set; }
 
         /// <summary>
         /// Determines if service can interact with desktop
         /// </summary>
-        public bool InteractWithDesktop { get; set; }
+        public bool InteractWithDesktop { get; internal set; }
 
         /// <summary>
         /// Fully qualified path to the service binary file
         /// </summary>
-        public string Path { get; set; }
+        public string Path { get; internal set; }
 
         /// <summary>
         /// System Error Code, if equals to 1066,
         /// the value of exit code is in ServiceSpecificExitCode property
         /// </summary>
-        public uint ExitCode { get; set; }
+        public uint ExitCode { get; internal set; }
 
         /// <summary>
         /// Custom exit code from service
         /// </summary>
-        public uint ServiceSpecificExitCode { get; set; }
+        public uint ServiceSpecificExitCode { get; internal set; }
 
         /// <summary>
         /// Determines if service can be stopped now
         /// </summary>
-        public bool CanStop { get; set; }
+        public bool CanStop { get; internal set; }
 
         /// <summary>
         /// Determines if service can be paused now
         /// </summary>
-        public bool CanPause { get; set; }
+        public bool CanPause { get; internal set; }
 
         /// <summary>
         /// List of services names that must start before this service will start
         /// </summary>
-        public ICollection<string> ServiceDependencies { get; set; }
+        public ICollection<string> ServiceDependencies { get; internal set; }
     }
 }
