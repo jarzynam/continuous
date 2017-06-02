dcf9bcf9be1757a53db972d38f34269171395883
Michał Jarzyna <jarzyna.michal@hotmail.com>
2017-06-02 22:26:06 +0200

    added compatibility tests for user

---
+++ src/Compatibility.Tests/Continuous.Test.User/Continuous.Test.User/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Continuous.Test.User")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Continuous.Test.User")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("863f36cf-7151-4013-990e-64f2f9e81a81")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
