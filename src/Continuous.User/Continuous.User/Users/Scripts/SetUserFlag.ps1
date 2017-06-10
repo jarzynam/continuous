85600e05b4c4f977f78b9cb4a60e45f26070cce6
Michał Jarzyna <jarzyna.michal@hotmail.com>
2017-06-11 01:06:26 +0200

    added missing file

---
+++ src/Continuous.User/Continuous.User/Users/Scripts/SetUserFlag.ps1
@@ -0,0 +1,17 @@
+﻿#  
+# change user flag
+#
+param([string]$name, [int] $flag, [bool] $value)
+
+$user = [ADSI]("WinNT://./$name, user")
+
+if($value -eq $true)
+{
+	$user.UserFlags.Value = $user.UserFlags.Value -bor $flag;
+}
+else
+{
+	$user.UserFlags.Value = $user.UserFlags.Value -bxor $flag;
+}
+
+$user.SetInfo();
